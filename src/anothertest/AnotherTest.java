ed73e24ba0163bb56365acce4d4923b374286efa
Kalogiannis Xristos <xristos_kalogiannis@yahoo.com>
2019-09-04 20:39:40 +0300

    Convert temperature from Fahrenheit to Celsius degree

---
+++ src/anothertest/AnotherTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anothertest;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import static javafx.beans.binding.Bindings.or;
+
+
+public class AnotherTest {
+
+  
+
+    
+    public static void main(String[] args) {
+        
+       
+  
+        System.out.println();
+  
+        
+  
+  
+    }
+
+        
+        
+        
+}
